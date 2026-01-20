f4cccba9b096cd0d30242906bc6555b4f77442c8
Nina Bobyleva <2847171@mail.ru>
2026-01-20 21:35:40 +0300

    cor

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,6 @@ import { productsReducer } from './features/productsSlice';
 import { userReducer } from './features/userSlice.js';
 import { cartReducer } from './features/cartSlice';
 
-// export const selectProducts = (state) => state.products.products;
 export const selectFilterProducts = (state) => state.products.filterProducts;
 export const selectCart = (state) => state.cart.cart;
 export const selectCartCount = (state) => state.cart.cartCount;
